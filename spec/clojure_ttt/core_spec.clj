c01cfb7e49dfc9306e855f82e7fff6a53207f72c
Matt Higgins <matthew.alan.higgins@gmail.com>
2016-06-14 15:48:34 -0700

    Refactors core_spec, pulling out some repetitive vars for ease of reading

--- spec/clojure_ttt/core_spec.clj
+++ spec/clojure_ttt/core_spec.clj
@@ -1,37 +1,44 @@
 (ns clojure-ttt.core-spec
   (:require [speclj.core :refer :all]
-            [clojure-ttt.core :refer :all]))
+    [clojure-ttt.core :refer :all]))
+(describe "Modeling a tic tac toe board"
 
-(describe "generate-new-board"
-  (it "creates a new board with the correct number of spaces"
-    (should= 9 (count (generate-new-board 3)))))
+  (before-all
+    (def new-board (generate-new-board 3))
+    (def board-with-0-marked-with-x (mark-space new-board 0 "x")))
 
-(describe "look-up-space"
-  (it "returns the marker that is at a given space"
-    (should= nil (look-up-space (generate-new-board 3) 0))))
+  (describe "generate-new-board"
+    (it "creates a new board with the correct number of spaces"
+      (should= 9 (count new-board))))
 
-(describe "mark-space"
-  (it "marks a space with the given mark"
-    (should= {0 {:marked "x"}, 1 {}, 2 {}, 3 {}, 4 {}, 5 {}, 6 {}, 7 {}, 8 {}} (mark-space (generate-new-board 3) 0 "x"))))
+  (describe "look-up-space"
+    (it "returns the marker that is at a given space"
+      (should= nil (look-up-space new-board 0))))
 
-(describe "find-free-spaces"
-  (it "returns the board with only the free spaces left"
-    (should= {1 {}, 2 {}, 3 {}, 4 {}, 5 {}, 6 {}, 7 {}, 8 {}} (find-free-spaces (mark-space (generate-new-board 3) 0 "x")))))
+  (describe "mark-space"
+    (it "marks a space with the given mark"
+      (should= board-with-0-marked-with-x (mark-space new-board 0 "x"))))
 
-(describe "find-taken-spaces"
-  (it "returns the board with only the taken spaces"
-  (should= {0 {:marked "x"}} (find-taken-spaces (mark-space (generate-new-board 3) 0 "x")))))
+  (describe "find-free-spaces"
+    (it "returns the board with only the free spaces left"
+      (should= {1 {}, 2 {}, 3 {}, 4 {}, 5 {}, 6 {}, 7 {}, 8 {}} (find-free-spaces (mark-space new-board 0 "x")))))
 
-(describe "board-full?"
-  (it "returns a false for a new board"
-    (should= false (board-full? (generate-new-board 3))))
+  (describe "find-taken-spaces"
+    (it "returns the board with only the taken spaces"
+      (should= {0 {:marked "x"}} (find-taken-spaces (mark-space new-board 0 "x")))))
 
-  (it "returns true for a board with no free spaces"
-    (should= true (board-full? {0 {:marked "x"}, 1 {:marked "x"}, 2 {:marked "x"}, 3 {:marked "x"}, 4 {:marked "x"}, 5 {:marked "x"}, 6 {:marked "x"}, 7 {:marked "x"}, 8 {:marked "x"}}))))
+  (describe "board-full?"
+    (it "returns a false for a new board"
+      (should= false (board-full? new-board)))
 
-(describe "space-free?"
-  (it "returns true if a space is empty"
-    (should= true (space-free? (generate-new-board 3) 0)))
+    (it "returns true for a board with no free spaces"
+      (should= true (board-full? {0 {:marked "x"}, 1 {:marked "x"}, 2 {:marked "x"}, 3 {:marked "x"}, 4 {:marked "x"}, 5 {:marked "x"}, 6 {:marked "x"}, 7 {:marked "x"}, 8 {:marked "x"}}))))
 
-  (it "returns false if a space is taken"
-    (should= false (space-free? {0 {:marked "x"}, 1 {}, 2 {}, 3 {}, 4 {}, 5 {}, 6 {}, 7 {}, 8 {}} 0))))
+  (describe "space-free?"
+    (it "returns true if a space is empty"
+      (should= true (space-free? new-board 0)))
+
+    (it "returns false if a space is taken"
+      (should= false (space-free? board-with-0-marked-with-x 0)))))
+
+(run-specs)
