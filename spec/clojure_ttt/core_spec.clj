4662d319007c2d391dc7a43d656d6534ba833168
Matt Higgins <matthew.alan.higgins@gmail.com>
2016-06-14 15:00:32 -0700

    Adds functions for creating new board, looking up a space, marking a space, and finding free spaces

--- spec/clojure_ttt/core_spec.clj
+++ spec/clojure_ttt/core_spec.clj
@@ -2,6 +2,18 @@
   (:require [speclj.core :refer :all]
             [clojure-ttt.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 1 1)))
+(describe "generate-new-board"
+  (it "creates a new board with the correct number of spaces"
+    (should= 9 (count (generate-new-board 3)))))
+
+(describe "look-up-space"
+  (it "returns the marker that is at a given space"
+    (should= nil (look-up-space (generate-new-board 3) 0))))
+
+(describe "mark-space"
+  (it "marks a space with the given mark"
+    (should= {0 {:marked "x"}, 1 {}, 2 {}, 3 {}, 4 {}, 5 {}, 6 {}, 7 {}, 8 {}} (mark-space (generate-new-board 3) 0 "x"))))
+
+(describe "find-free-spaces"
+  (it "returns the board with only the free spaces left"
+    (should= {1 {}, 2 {}, 3 {}, 4 {}, 5 {}, 6 {}, 7 {}, 8 {}} (find-free-spaces (mark-space (generate-new-board 3) 0 "x")))))
--- src/clojure_ttt/core.clj
+++ src/clojure_ttt/core.clj
@@ -1,1 +1,18 @@
 (ns clojure-ttt.core)
+
+(defn generate-new-board
+  [num-of-rows]
+  (let [num-of-spaces (* num-of-rows num-of-rows)]
+    (reduce #(assoc %1 %2 {}) {} (take num-of-spaces (range)))))
+
+(defn look-up-space
+  [board space]
+  (get-in board [space :marked]))
+
+(defn mark-space
+  [board space mark]
+  (assoc board space {:marked mark}))
+
+(defn find-free-spaces
+  [board]
+  (into {} (filter #(nil? (look-up-space board (first %))) board)))
