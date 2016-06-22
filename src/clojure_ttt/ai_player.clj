79b10ea811c36870a2b5fbcbdeb14014dff2727d
Matt Higgins <matthew.alan.higgins@gmail.com>
2016-06-22 14:38:32 -0700

    Adds small readability improvements to ai-player and player

--- src/clojure_ttt/ai_player.clj
+++ src/clojure_ttt/ai_player.clj
@@ -13,8 +13,8 @@
 
 (defn alternate-max-and-min
   [function]
-  (if
-    (= function get-max-value) get-min-value
+  (if (= function get-max-value)
+    get-min-value
     get-max-value))
 
 (defn all-map-values-are-integers?
@@ -26,24 +26,25 @@
     (reduce (fn [new-map [key value]]
       (cond
         (integer? value) (assoc new-map key value)
-        (and (map? value) (= 1 (count value))) (assoc new-map key (second (first value)))
+        (and (map? value) (= 1 (count value))) (assoc new-map key ((comp second first) value))
         (all-map-values-are-integers? value) (assoc new-map key (max-or-min-function value))
-        :else (assoc new-map key (find-best-score value (alternate-max-and-min max-or-min-function))))) {} starting-map))
+        :else (assoc new-map key (find-best-score value (alternate-max-and-min max-or-min-function)))))
+    {} starting-map))
 
 (defn flatten-score-map
   [scores]
-  (if
-    (all-map-values-are-integers? scores) scores
+  (if (all-map-values-are-integers? scores)
+    scores
     (let [new-map (find-best-score scores get-min-value)]
       (recur new-map))))
 
 (defn score-board
   [board marker depth]
   (let [winner (core/has-won? board)]
-    (cond
-      (= false winner) 0
-      (= winner marker) (- 10 depth)
-      :else (+ depth -10))))
+    (condp = winner
+      false 0
+      marker (- 10 depth)
+      (+ depth -10))))
 
 (defn play-next-round-of-moves
   [board depth marker color free-spaces]
--- src/clojure_ttt/player.clj
+++ src/clojure_ttt/player.clj
@@ -9,14 +9,14 @@
 (deftype ComputerPlayer []
   Player
   (get-move [type board marker]
-    (let [moves-and-scores (ai-player/negamax board 0 marker 1)
-          potential-moves (ai-player/flatten-score-map moves-and-scores)]
-      (first (last (sort-by val potential-moves))))))
+    (let [all-game-possibilities (ai-player/negamax board 0 marker 1)
+          all-possible-next-moves-and-scores (ai-player/flatten-score-map all-game-possibilities)]
+      ((comp first last) (sort-by val all-possible-next-moves-and-scores)))))
 
 (deftype HumanPlayer [io-channel]
   Player
   (get-move [type board _]
-            (get-move type board _ (str "Where would you like to play? Please enter a number between 0 and " (int (dec (count board))))))
+            (get-move type board _ (str "Where would you like to play? Please enter a number between 0 and " ((comp int dec count) board))))
   (get-move [type board _ message]
             (loop [type type
                    board board
@@ -25,4 +25,4 @@
             (let [response (console-ui/get-user-input io-channel message)]
               (if (core/valid-move? board response)
                 (Integer/parseInt response)
-                (recur type board _ (str "Sorry that's not a valid move. Please enter a number between 0 and " (int (dec (count board)))" that isn't already taken.")))))))
+                (recur type board _ (str "Sorry that's not a valid move. Please enter a number between 0 and " ((comp int dec count) board) " that isn't already taken.")))))))
